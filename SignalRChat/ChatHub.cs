d643d4b6c2666676f00e5ea6ec6a20f67d3232d7
joaopaulo2391 <j_paulo_15@hotmail.com>
2018-04-07 09:55:03 -0300

    Chatbox with MS SignalR

---
+++ SignalRChat/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace SignalRChat
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+            
+
+
+        }
+    }
+}
---
+++ SignalRChat/Scripts/Chat.js
@@ -0,0 +1,19 @@
+﻿(function () {
+
+    $('#live-chat header').on('click',
+        function () {
+
+            $('.chat').slideToggle(300, 'swing');
+            $('.chat-message-counter').fadeToggle(300, 'swing');
+
+        });
+
+    $('.chat-close').on('click',
+        function (e) {
+
+            e.preventDefault();
+            $('#live-chat').fadeOut(300);
+
+        });
+
+})();
